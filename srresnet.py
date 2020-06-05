67eb6db7180dc1656a524b8474b4c5eda6193b49
ben <pingheng.chang@gmail.com>
2020-06-05 20:52:48 +0800

    fix Prelu function bug

--- srresnet.py
+++ srresnet.py
@@ -19,7 +19,7 @@ class Srresnet:
 
     def _Prelu(self, _x):
         alphas = tf.get_variable('alpha', _x.get_shape()[-1],\
-            initializer=tf.glorot_normal_initializer(),
+            initializer=tf.constant_initializer(0.0),
             dtype=tf.float32)
         pos = tf.nn.relu(_x)
         neg = alphas * (_x - abs(_x)) * 0.5
