8dc4d256293706c9294d851287e81cda1c885c41
ben <pingheng.chang@gmail.com>
2020-07-01 03:55:06 +0800

    fixing keep checkpoint to 100

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def main():
         iteration = 0
         epoch = 0
 
-        saver = tf.train.Saver(max_to_keep=50)
+        saver = tf.train.Saver(max_to_keep=100)
 
         # Load all
         if args.load:
