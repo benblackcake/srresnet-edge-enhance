e2312d6bd8737c091a773afc4d1b5fb1eb504f29
ben <pingheng.chang@gmail.com>
2020-07-16 17:13:59 +0800

    debuging color issue

--- utils.py
+++ utils.py
@@ -68,7 +68,7 @@ def sobel_direct_oper(image):
     # y_edge = np.expand_dims(y_edge,axis=-1)
     # diagonal_edge = np.expand_dims(diagonal_edge,axis=-1)
 
-    result = np.stack([image,x_edge,y_edge,diagonal_edge], axis=-1) #[:,:,3]
+    result = np.stack([x_edge,y_edge,diagonal_edge], axis=-1) #[:,:,3]
 
     # print(result.shape)
 
@@ -83,7 +83,7 @@ def sobel_oper_batch(batch):
     return sobeled
 
 def sobel_direct_oper_batch(batch):
-    sobeled = np.zeros((batch.shape[0], batch.shape[1] , batch.shape[2],4))
+    sobeled = np.zeros((batch.shape[0], batch.shape[1] , batch.shape[2]))
     for i in range(batch.shape[0]):
         sobeled[i,:,:,:] = sobel_direct_oper(batch[i,:,:])
 
@@ -148,12 +148,12 @@ def evaluate_model(loss_function, get_batch, sess, num_images, batch_size):
         batch_hr = batch_bgr2rgb(get_batch)
         dwt_rgb = batch_dwt(batch_hr)
         dwt_rgb = np.clip(np.abs(dwt_rgb), 0, 255).astype('uint8')
-        # dwt_r_BCD = dwt_rgb[:,:,:,1:4]
-        # dwt_g_BCD = dwt_rgb[:,:,:,5:8]
-        # dwt_b_BCD = dwt_rgb[:,:,:,9:12]
+        dwt_r_BCD = dwt_rgb[:,:,:,1:4]
+        dwt_g_BCD = dwt_rgb[:,:,:,5:8]
+        dwt_b_BCD = dwt_rgb[:,:,:,9:12]
 
-        # dwt_label = np.concatenate([dwt_r_BCD, dwt_g_BCD, dwt_b_BCD], axis=-1)/255.
-        dwt_label = dwt_rgb
+        dwt_label = np.concatenate([dwt_r_BCD, dwt_g_BCD, dwt_b_BCD], axis=-1)
+        # dwt_label = dwt_rgb
 
         sobeled_batch_r = sobel_direct_oper_batch(dwt_rgb[:,:,:,0])
         sobeled_batch_g = sobel_direct_oper_batch(dwt_rgb[:,:,:,4])
@@ -163,7 +163,7 @@ def evaluate_model(loss_function, get_batch, sess, num_images, batch_size):
         # sobeled_batch_g = np.concatenate([sobeled_batch_g,np.expand_dims(dwt_rgb[:,:,:,4], axis=-1)],axis=-1)
         # sobeled_batch_b = np.concatenate([sobeled_batch_b,np.expand_dims(dwt_rgb[:,:,:,8], axis=-1)],axis=-1)
 
-        sobeled_train = np.concatenate([sobeled_batch_r,sobeled_batch_g,sobeled_batch_b],axis=-1)/255. # Normalized
+        sobeled_train = np.concatenate([sobeled_batch_r,sobeled_batch_g,sobeled_batch_b],axis=-1) # Normalized
 
 
 
