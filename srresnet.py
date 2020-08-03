3195e28fe0ed7b3714ea0d2ccb8d8ac7635eb812
ben <pingheng.chang@gmail.com>
2020-08-04 01:20:36 +0800

    add srcnn

--- srresnet.py
+++ srresnet.py
@@ -170,39 +170,59 @@ class Srresnet:
             return x_conv_out
 
 
-    def forward_edge_branch(self, x_BCD):
+    def forward_edge_branch(self, x_BCD, net='RDN'):
         '''
         Args:
             x_BCD: input SWT(LH,HL,HH) 
         '''
-        with tf.variable_scope('srresnet_edge_branch',reuse=tf.AUTO_REUSE) as scope:
-            weights = {
-                # 'w_resnet_in': tf.Variable(tf.random_normal([9, 9, 9, 64], stddev=1e-3), name='w_resnet_in'),
-                # 'w_resnet_1': tf.Variable(tf.random_normal([3, 3, 64, 64], stddev=1e-3), name='w_resnet_1'),
-                # 'w_resnet_out': tf.Variable(tf.random_normal([9, 9, 64, 9], stddev=1e-3), name='w_resnet_out'),
-                'w_RDB_in': tf.Variable(tf.random_normal([9, 9, 9, 64], stddev=1e-2), name='w_resnet_in'),
-                'w_RDB_1': tf.Variable(tf.random_normal([9, 9, 1024, 64], stddev=1e-2), name='w_resnet_in'),
-                'w_RDB_out': tf.Variable(tf.random_normal([9, 9, 64, 9], stddev=1e-2), name='w_resnet_in'),
+        if net == 'RDN':
+            with tf.variable_scope('srresnet_edge_branch',reuse=tf.AUTO_REUSE) as scope:
+                weights = {
+                    # 'w_resnet_in': tf.Variable(tf.random_normal([9, 9, 9, 64], stddev=1e-3), name='w_resnet_in'),
+                    # 'w_resnet_1': tf.Variable(tf.random_normal([3, 3, 64, 64], stddev=1e-3), name='w_resnet_1'),
+                    # 'w_resnet_out': tf.Variable(tf.random_normal([9, 9, 64, 9], stddev=1e-3), name='w_resnet_out'),
+                    'w_RDB_in': tf.Variable(tf.random_normal([9, 9, 9, 64], stddev=1e-2), name='w_resnet_in'),
+                    'w_RDB_1': tf.Variable(tf.random_normal([9, 9, 1024, 64], stddev=1e-2), name='w_resnet_in'),
+                    'w_RDB_out': tf.Variable(tf.random_normal([9, 9, 64, 9], stddev=1e-2), name='w_resnet_in'),
 
-            }
-            self._weightsR, self._biasesR = self.RDBParams()
-            x_BCD = tf.nn.conv2d(x_BCD, weights['w_RDB_in'], strides=[1,1,1,1], padding='SAME')
-            x_skip = x_BCD
+                }
+                self._weightsR, self._biasesR = self.RDBParams()
+                x_BCD = tf.nn.conv2d(x_BCD, weights['w_RDB_in'], strides=[1,1,1,1], padding='SAME')
+                x_skip = x_BCD
 
-            x_BCD = self.RDBs(x_BCD)
+                x_BCD = self.RDBs(x_BCD)
 
-            print('__RDB__out',x_BCD)
+                print('__RDB__out',x_BCD)
 
-            x_BCD = tf.nn.conv2d(x_BCD, weights['w_RDB_1'], strides=[1,1,1,1], padding='SAME')
-            x_BCD =  tf.contrib.keras.layers.PReLU(shared_axes=[1, 2])(x_BCD)
+                x_BCD = tf.nn.conv2d(x_BCD, weights['w_RDB_1'], strides=[1,1,1,1], padding='SAME')
+                x_BCD =  tf.contrib.keras.layers.PReLU(shared_axes=[1, 2])(x_BCD)
 
-            x_BCD = tf.add(x_skip,x_BCD)
-            for i in range(self.num_upsamples):
-                x_BCD = self.Upsample2xBlock(x_BCD, kernel_size=3, in_channel=64, filter_size=256)
+                x_BCD = tf.add(x_skip,x_BCD)
+                for i in range(self.num_upsamples):
+                    x_BCD = self.Upsample2xBlock(x_BCD, kernel_size=3, in_channel=64, filter_size=256)
 
-            x_BCD_out = tf.nn.conv2d(x_BCD, weights['w_RDB_out'], strides=[1,1,1,1], padding='SAME')
+                x_BCD_out = tf.nn.conv2d(x_BCD, weights['w_RDB_out'], strides=[1,1,1,1], padding='SAME')
 
-            return x_BCD_out
+                return x_BCD_out
+
+        if net == 'srcnn':
+            with tf.variable_scope('srresnet_edge_branch',reuse=tf.AUTO_REUSE) as scope:
+
+                weights = {
+                    'w1': tf.Variable(tf.random_normal([9, 9, 9, 64], stddev=1e-3), name='w1'),
+                    'w2': tf.Variable(tf.random_normal([1, 1, 64, 32], stddev=1e-3), name='w2'),
+                    'w3': tf.Variable(tf.random_normal([5, 5, 32, 9], stddev=1e-3), name='w3')
+                }
+
+                biases = {
+                    'b1': tf.Variable(tf.zeros([64], name='b1')),
+                    'b2': tf.Variable(tf.zeros([32], name='b2')),
+                    'b3': tf.Variable(tf.zeros([9], name='b3'))
+                }
+                conv1 = tf.nn.relu(tf.nn.conv2d(x_BCD, weights['w1'], strides=[1,1,1,1], padding='VALID') + biases['b1'])
+                conv2 = tf.nn.relu(tf.nn.conv2d(conv1, weights['w2'], strides=[1,1,1,1], padding='VALID') biases['b2'])
+                conv3 = tf.nn.conv2d(conv2, weights['w3'], strides=[1,1,1,1], padding='VALID') + biases['b3'] # This layer don't need ReLU
+
 
     def _content_loss(self, y_A, y_A_pred, y_BCD, y_BCD_pred):
 
